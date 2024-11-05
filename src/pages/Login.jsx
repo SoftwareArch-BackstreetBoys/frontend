a6a83942341ff2c6c4cd72433706df71ac4b6e45
LittleNa1000 <naphanccnp@gmail.com>
2024-11-06 03:06:28 +0700

    feat: implement goole oauth in /login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,32 @@
-import React from 'react';
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button"
+import { useGoogleLogin } from "@react-oauth/google"
 
 const Login = () => {
-  const handleOnClick = () => {
-    const callbackUrl = window.location.origin + '/events';
-    window.location.href = `${process.env.GOOGLE_OAUTH_ROUTE}/auth/login?callbackUrl=${callbackUrl}`;
-  }
+  const googleLogin = useGoogleLogin({
+    onSuccess: async (tokenResponse) => {
+      console.log("Google login successful", tokenResponse)
+      const response = await fetch(
+        `${process.env.GOOGLE_OAUTH_ROUTE}/auth/login/callback?token=${tokenResponse.access_token}`
+      )
+      console.log("Google login response", await response.json())
+
+      // You can now use the tokenResponse to authenticate the user in your app
+    },
+    onError: (error) => {
+      console.error("Google login failed", error)
+      // Handle login errors here
+    },
+  })
 
   return (
     <div className="max-w-md mx-auto">
       <h2 className="text-2xl font-bold mb-4">Login</h2>
-      <Button onClick={handleOnClick} className="w-full">Login with Google</Button>
+      <Button onClick={googleLogin} className="w-full">
+        Login with Google
+      </Button>
       {/* Add more login logic here */}
     </div>
-  );
-};
+  )
+}
 
-export default Login;
+export default Login
